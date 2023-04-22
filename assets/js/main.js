d5040757f7711ba5c58fab943ee97b2e20091079
Jimwell <jimwell.rabino@thynker.tech>
2023-04-22 20:54:43 +0800

    password toggle

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+    const passwordInput = document.getElementById('password-input');
+    const passwordToggle = document.getElementById('password-toggle');
+
+    passwordToggle.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        
+        this.classList.toggle('bi-eye-slash-fill');
+        this.classList.toggle('bi-eye-fill');
+    });
+
+    passwordInput.addEventListener('input', function() {
+        if (passwordInput.value === '') {
+          passwordToggle.style.display = 'none';
+        } else {
+          passwordToggle.style.display = 'block';
+        }
+      });
